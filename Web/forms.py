0598758bc25e17b47ae962217281b8ed30d9f41e
Carles Llobet <krow.boarder.9@gmail.com>
2016-12-02 19:15:49 +0100

    SALVA NO TOQUIS RES MES; SOLO MIRA!

---
+++ Web/forms.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_ipv4_address, EmailValidator, validate_email
+from django.utils.translation import ugettext_lazy as _
+
+my_default_errors_Name = {
+    'required': _(u'Especifica un nom de tasca. ')
+}
+
+
+class formCoche(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(formCoche, self).__init__(*args, **kwargs)
+
+    name = forms.CharField(widget=forms.TextInput(attrs={"label": 'Name:', "max_length": 100, "class": "form-control",
+                                                         "placeholder": 'Nom de la tasca'}),
+                           error_messages=my_default_errors_Name)
+
+    verbosity = forms.IntegerField(label='verbosity')
+    level = forms.IntegerField(label='level')
+    risk = forms.IntegerField(label='risk')
+    depth = forms.IntegerField(label='depth')
+    mail = forms.BooleanField(label='mail', initial=False, required=False)
+    mail_field = forms.CharField(widget=forms.TextInput(attrs={"max_length": 100, "class": "form-control"}),
+                                 required=False, validators=[validate_email])
+
+    # Periodicitat
+    periodicity_checkbox = forms.BooleanField(label='periodicity', initial=False, required=False)
+    periodicity = forms.ChoiceField(widget=forms.Select(attrs={"label": 'Periodicitat:'}), choices=Periodicities,
+                                    validators=[validate_periodicity])
+
+    execute_date = forms.DateTimeField(label='execute_date', required=False, widget=DateTimePicker(
+        options={"format": "YYYY-MM-DD HH:mm", "pickSeconds": False}))
+
+    def clean(self):
+        cleaned_data = super(formCoche, self).clean()
+        url = cleaned_data.get("url")
+        user = cleaned_data.get("user")
+        password = cleaned_data.get("password")
+        ip = cleaned_data.get("ip")
+        port = cleaned_data.get("port")
+        db_name = cleaned_data.get("db_name")
+        verbosity = cleaned_data.get("verbosity")
+        level = cleaned_data.get("level")
+        risk = cleaned_data.get("risk")
+        depth = cleaned_data.get("depth")
+
+        print port
+        raise forms.ValidationError(
+            _("Siusplau especifica com a minim 1 target, o per URL o per connexio directa a Base de Dades"))
+        if url == "" and user == "" and (password != "" or ip != "" or port != None or db_name != ""):
+            self.add_error('user', (_("Especifica un nom d'usuari")))
+        if url == "" and password == "" and (user != "" or ip != "" or port != None or db_name != ""):
+            self.add_error('password', (_("Especifica una contrasenya")))
+        if url == "" and ip == "" and (password != "" or user != "" or port != None or db_name != ""):
+            self.add_error('ip', (_("Especifica una direccio IP")))
+        if url == "" and port == None and (password != "" or ip != "" or user != "" or db_name != ""):
+            self.add_error('port', (_("Especifica un port")))
+        if url == "" and db_name == "" and (password != "" or ip != "" or port != None or user != ""):
+            self.add_error('db_name', (_("Especifica un nom de base de dades")))
+        if verbosity < 0 or verbosity > 6 or level < 1 or level > 5 or risk < 1 or risk > 3 or depth < 0 or depth > 10:
+            print str(verbosity)
+            ", "
+            str(level)
+            ", "
+            str(risk)
+            ", "
+            str(depth)
+            raise forms.ValidationError(
+                _("Els camps level, verbosity, risk, i depth no poden tenir valors fora del rang del slider"))
--- Web/views.py
+++ Web/views.py
@@ -1,20 +1,81 @@
+import datetime
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template import Context
+
+from Web import forms
+from Web import models
+
 
 def index(request):
     return render(request, 'index.html')
 
+
 def asnef(request):
     return render(request, 'asnef.html')
 
+
 def coche(request):
-    return render(request, 'coche.html')
+    context = Context({})
+
+    if request.method == 'POST':
+        form = forms.formCoche(request.POST)
+        if form.is_valid():
+            # url
+            target_url = form.cleaned_data['url']
+            # direct connection
+            target_dbms = form.cleaned_data['dbms']
+            target_user = form.cleaned_data['user']
+            target_password = form.cleaned_data['password']
+            target_ip = form.cleaned_data['ip']
+            target_port = form.cleaned_data['port']
+            target_db_name = form.cleaned_data['db_name']
+            # parameters
+            c = form.cleaned_data['charset']
+            v = form.cleaned_data['verbosity']
+            l = form.cleaned_data['level']
+            r = form.cleaned_data['risk']
+            d = form.cleaned_data['depth']
+            n = form.cleaned_data['name']
+            m = form.cleaned_data['mail']
+            mf = form.cleaned_data['mail_field']
+            pc = form.cleaned_data['periodicity_checkbox']
+            ed = form.cleaned_data['execute_date']
+            pd = form.cleaned_data['periodicity']
+
+            p = models.sqlmap_requests.objects.create(name=n, level=l, verbosity=v, risk=r,
+                                                      depth=d,
+                                                      charset=c, user=request.user,
+                                                      mail=mf, execute_date=ed, periodicity=pd)
+            return HttpResponseRedirect('/')
+        else:
+            print form.errors
+    else:
+        form = forms.formCoche()
+        context.update({"form": form})
+        lastNum = models.expediente.objects.all().order_by("numexp").reverse[0].values("numexp")
+        fecha = datetime.now()
+        if not lastNum:
+            lastNum = 40000
+        else:
+            lastNum = 1
+        context.update({"numexp": lastNum})
+        context.update({"data": fecha.year+"-"+fecha.month+"-"+fecha.day})
+        context.update({"hora": fecha.hour+":"+fecha.minute+":"+fecha.second})
+        return render(request, 'coche.html', context)
+
 
 def hipotecario(request):
-    return render(request, 'hipotecario.html')
+    context = Context({})
+    return render(request, 'hipotecario.html', context)
+
 
 def microcredito(request):
-    return render(request, 'microcredito.html')
+    context = Context({})
+    return render(request, 'microcredito.html', context)
+
 
 def personal(request):
-    return render(request, 'personal.html')
-
+    context = Context({})
+    return render(request, 'personal.html', context)
