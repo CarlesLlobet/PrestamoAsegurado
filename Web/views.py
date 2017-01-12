268c6c4d87c8faf6339c13477a2a300ffa3864f4
Salva <salva@Salvas-iMac.local>
2017-01-12 14:24:17 +0100

    NEW

--- Web/views.py
+++ Web/views.py
@@ -33,22 +33,190 @@ def index(request):
     return render(request, 'web_index.html', context)
 
 def faq(request):
-    return render(request, 'web_faq.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_faq.html', context)
 
 def servicios(request):
-    return render(request, 'web_services.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_services.html', context)
 
 def empresa(request):
-    return render(request, 'web_empresa.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_empresa.html', context)
 
 def politica(request):
-    return render(request, 'web_politica.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_politica.html', context)
 
 def aviso(request):
-    return render(request, 'web_aviso.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_aviso.html', context)
 
 def gastos(request):
-    return render(request, 'web_gastos.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_gastos.html', context)
 
 def cookies(request):
-    return render(request, 'web_cookies.html')
+    context = {}
+    logout(request)
+    if request.method == "POST":
+        form = forms.formLogin(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            passwd = form.cleaned_data['password']
+            usr = authenticate(username=user, password=passwd)
+            if usr is not None:
+                if usr.is_active:
+                    login(request, usr)
+                    if user == "operadora":
+                        return HttpResponseRedirect('/formularios')
+                    else:
+                        return HttpResponseRedirect(
+                            '/formularios')  # TODO: Canviar pel lloc on van els users a penjar fotos
+            else:
+                form = forms.formLogin()
+                context.update({"incorrect": "incorrect"})
+                context.update({"form": form})
+                return render(request, 'web_index.html', context)
+    else:
+        form = forms.formLogin()
+        context.update({"form": form})
+    return render(request, 'web_cookies.html', context)
